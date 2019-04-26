68b65141564a3912e16ef883ff1244e3a4fb6d2b
Hasan <realestsanibo@gmail.com>
2019-04-25 17:39:05 -0700

    Update website

---
+++ main.js
@@ -0,0 +1,10 @@
+let canvas = SVG('canvas').size(600, 500);
+let square = canvas. rect (50,500) 
+square.fill('red');
+square.move(315,200);
+let circle = canvas.circle(100)
+circle.fill ('gold');
+circle.move(290,144) 
+let ellipse = canvas. ellipse (200,100) 
+ellipse.fill('turquoise');
+ellipse.move(110,150)
